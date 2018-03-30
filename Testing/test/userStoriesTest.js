fea88314cca51e6a4b297f0fa45b0e56d6b1e5bf
Pantuflas <antoniojss94@gmail.com>
2018-03-30 14:15:50 -0600

    Added the user-stories file- Test for CREATE complete

--- Testing/test/userStoriesTest.js
+++ Testing/test/userStoriesTest.js
@@ -35,13 +35,7 @@ describe('User_story model', () => {
             let postOptions = {
                 url: URL + '/user-stories',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ 
-                    weight: '2', 
-                    scrum_board_status: '1', 
-                    description: 'This is a description', 
-                    priority: '2', 
-                    sprint_id: '1', 
-                    project_id: '1'})
+                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: '1', project_id: '1'})
             };
 
             // Make post request
@@ -59,11 +53,11 @@ describe('User_story model', () => {
             });
         });
 
-        // null query parameter, expects unsuccessful insertion
+        // null weight parameter, expects unsuccessful insertion
         it('Null weight', (done) => {
             // Define POST request parameters and body
             let postOptions = {
-                url: URL + '/user_stories',
+                url: URL + '/user-stories',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ weight: null, scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: '1', project_id: '1'})
             };
@@ -90,7 +84,7 @@ describe('User_story model', () => {
         it('Null scrum_board_status', (done) => {
             // Define POST request parameters and body
             let postOptions = {
-                url: URL + '/user_stories',
+                url: URL + '/user-stories',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ weight: '2', scrum_board_status: null, description: 'This is a description', priority: '2', sprint_id: '1', project_id: '1'})
             };
@@ -101,39 +95,11 @@ describe('User_story model', () => {
                 let newUser_story = postOptions.body;
 
                 // Make get request to check that the object was not inserted
-                request.get( URL + 'user_stories', (error, response, body) => {
+                request.get( URL + 'user-stories', (error, response, body) => {
                     expect(response.statusCode).to.be.equal(200); // if response is successful
                     
                     let list = JSON.parse(body);
-                    let found = findLog(list, newUser_story);
-
-                    expect(found).to.be.false; // check that no coincidence is found
-                });
-
-                done();
-            });
-        });
-
-        // Incorrect miembro_matricula parameter, expects unsuccessful insertion
-        it('Incorrect miembro matricula', (done) => {
-            // Define POST request parameters and body
-            let postOptions = {
-                url: URL + '/logs',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ query: 'INSERT INTO LOGS ...', date_time: '2018-03-27 19:31:00', miembro_matricula: 'B01324568'})
-            };
-
-            // Make post request
-            request.post(postOptions, (error, response, body) => {
-                expect(response.statusCode).to.be.equal(400); // response should be error 400
-                let newLog = postOptions.body;
-
-                // Make get request to check that the object was not inserted
-                request.get( URL + 'logs', (error, response, body) => {
-                    expect(response.statusCode).to.be.equal(200); // if response is successful
-                    
-                    let list = JSON.parse(body);
-                    let found = findLog(list, newLog);
+                    let found = findUserStory(list, newUser_story);
 
                     expect(found).to.be.false; // check that no coincidence is found
                 });
@@ -157,7 +123,7 @@ describe('User_story model', () => {
                 let newUser_story = postOptions.body;
 
                 // Make get request to check that the object was not inserted
-                request.get( URL + 'user_stories', (error, response, body) => {
+                request.get( URL + 'user-stories', (error, response, body) => {
                     expect(response.statusCode).to.be.equal(200); // if response is successful
                     
                     let list = JSON.parse(body);
@@ -170,58 +136,153 @@ describe('User_story model', () => {
             });
         });
 
+
+        // null priority, expects unsuccessful insertion
         it('Null priority', (done) => {
             // Define POST request parameters and body
             let postOptions = {
-                url: URL + '/user_stories',
+                url: URL + '/user-stories',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: null, sprint_id: '1', project_id: '1'})
             };
 
             // Make post request
             request.post(postOptions, (error, response, body) => {
-                expect(response.statusCode).to.be.equal(200); // if response is successful
-                let newLog = JSON.parse(body);
+                expect(response.statusCode).to.be.equal(400); // response should be error 400
+                let newUser_story = postOptions.body;
 
-                // Make get request to get the inserted object
-                request.get( URL + 'user_stories/' + newUser_story.id, (error, response, body) => {
+                // Make get request to check that the object was not inserted
+                request.get( URL + 'user-stories', (error, response, body) => {
                     expect(response.statusCode).to.be.equal(200); // if response is successful
-                    expect(newLog).to.deep.equal(JSON.parse(body)); // check that the object we created and the one obtain are equal
+                    
+                    let list = JSON.parse(body);
+                    let found = findUserStory(list, newUser_story);
+                    expect(found).to.be.false; // check that no coincidence is found
                 });
 
                 done();
             });
         });
 
-        it('valid request', (done) => {
+
+        // null sprint_id, expects unsuccessful insertion
+        it('Null sprint_id', (done) => {
             // Define POST request parameters and body
             let postOptions = {
-                url: URL + '/user_stories',
+                url: URL + '/user-stories',
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: '1', project_id: '1'})
+                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: null, project_id: '1'})
             };
 
             // Make post request
             request.post(postOptions, (error, response, body) => {
-                expect(response.statusCode).to.be.equal(200); // if response is successful
-                let newLog = JSON.parse(body);
+                expect(response.statusCode).to.be.equal(400); // response should be error 400
+                let newUser_story = postOptions.body;
 
-                // Make get request to get the inserted object
-                request.get( URL + 'user_stories/' + newUser_story.id, (error, response, body) => {
+                // Make get request to check that the object was not inserted
+                request.get( URL + 'user-stories', (error, response, body) => {
                     expect(response.statusCode).to.be.equal(200); // if response is successful
-                    expect(newLog).to.deep.equal(JSON.parse(body)); // check that the object we created and the one obtain are equal
+                    
+                    let list = JSON.parse(body);
+                    let found = findUserStory(list, newUser_story);
+                    expect(found).to.be.false; // check that no coincidence is found
                 });
 
                 done();
             });
         });
+
+        // Incorrect sprint_id parameter, expects unsuccessful insertion
+        it('Incorrect sprint_id', (done) => {
+            // Define POST request parameters and body
+            let postOptions = {
+                url: URL + '/user-stories',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: 'one', project_id: '1'})
+            };
+
+            // Make post request
+            request.post(postOptions, (error, response, body) => {
+                expect(response.statusCode).to.be.equal(400); // response should be error 400
+                let newUser_story = postOptions.body;
+
+                // Make get request to check that the object was not inserted
+                request.get( URL + 'user-stories', (error, response, body) => {
+                    expect(response.statusCode).to.be.equal(200); // if response is successful
+                    
+                    let list = JSON.parse(body);
+                    let found = findUserStory(list, newUser_story);
+
+                    expect(found).to.be.false; // check that no coincidence is found
+                });
+
+                done();
+            });
+        });
+
+        // null project_id, expects unsuccessful insertion
+        it('Null project_id', (done) => {
+            // Define POST request parameters and body
+            let postOptions = {
+                url: URL + '/user-stories',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: '1', project_id: null})
+            };
+
+            // Make post request
+            request.post(postOptions, (error, response, body) => {
+                expect(response.statusCode).to.be.equal(400); // response should be error 400
+                let newUser_story = postOptions.body;
+
+                // Make get request to check that the object was not inserted
+                request.get( URL + 'user-stories', (error, response, body) => {
+                    expect(response.statusCode).to.be.equal(200); // if response is successful
+                    
+                    let list = JSON.parse(body);
+                    let found = findUserStory(list, newUser_story);
+                    expect(found).to.be.false; // check that no coincidence is found
+                });
+
+                done();
+            });
+        });
+
+        // Incorrect project_id parameter, expects unsuccessful insertion
+        it('Incorrect project_id', (done) => {
+            // Define POST request parameters and body
+            let postOptions = {
+                url: URL + '/user-stories',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ weight: '2', scrum_board_status: '1', description: 'This is a description', priority: '2', sprint_id: '1', project_id: 'one'})
+            };
+
+            // Make post request
+            request.post(postOptions, (error, response, body) => {
+                expect(response.statusCode).to.be.equal(400); // response should be error 400
+                let newUser_story = postOptions.body;
+
+                // Make get request to check that the object was not inserted
+                request.get( URL + 'user-stories', (error, response, body) => {
+                    expect(response.statusCode).to.be.equal(200); // if response is successful
+                    
+                    let list = JSON.parse(body);
+                    let found = findUserStory(list, newUser_story);
+
+                    expect(found).to.be.false; // check that no coincidence is found
+                });
+
+                done();
+            });
+        });
+
+
     });
 
     //Retrieve opertion
     describe('Retrieve: #retrieve() | parameters: id', () => {
 
-        // Retrieve an existent log , a successfull response conataining the log
-        it('Retrieve existent log', (done) => {
+        // Retrieve an existent user_story , a successfull response conataining the user_story
+        it('Retrieve existent user_story', (done) => {
             let log = { id: 1, query: 'INSERT INTO logs', date_time: '2017-03-27 19:31:00', miembro_matricula: 'A01329447' };
 
             request.get(URL + '/logs/1', (error, response, body) => {
