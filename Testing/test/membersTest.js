20c7ce59b9c8b9e77c8f7d905f6ab61746f4bc1b
ChavaO777 <salvador@orozco.in>
2018-03-30 17:01:24 -0600

    Completed the tests for members/update

--- Testing/test/membersTest.js
+++ Testing/test/membersTest.js
@@ -285,25 +285,223 @@ describe('Member model', () => {
         // Try to update a member. Make the department_major field a non-existent value.
         it('Update a member with a non-existent department_major field', (done) => {
             
-            done();
+            request.get(URL + '/members/1', (error, response, body) => {
+
+                expect(response.statusCode).to.be.equal(200);
+
+                let oldMember = {
+
+                    id: body.id,
+                    department_major: body.department_major,
+                    name: body.name,
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let newMember = {
+
+                    id: body.id,
+                    department_major: "NON_EXISTENT_VALUE", //non-existent value of a department_major
+                    name: body.name,
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let putOptions = {
+
+                    url: URL + '/members',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(newMember)
+                }
+
+                // Make put request
+                request.put(putOptions, (error, response, body) => {
+
+                    expect(response.statusCode).to.be.equal(400);
+
+                    // Make a get request to verify that the object was not inserted
+                    request.get(URL + '/members', (error, response, body) => {
+
+                        expect(response.statusCode).to.be.equal(200); //If response is successful
+
+                        let list = JSON.parse(body);
+                        let found1 = findMember(list, newMember);
+                        expect(found1).to.be.false; //Check that the member was not updated
+
+                        let found2 = findMember(list, oldMember);
+                        expect(found2).to.be.true; //Check that the member with old values still exists
+                    });
+
+                    done();
+                });                
+            });
         });
 
         // Try to update a member. Make the name field an empty string.
         it('Update a member with an empty name field', (done) => {
             
-            done();
+            request.get(URL + '/members/1', (error, response, body) => {
+
+                expect(response.statusCode).to.be.equal(200);
+
+                let oldMember = {
+
+                    id: body.id,
+                    department_major: body.department_major,
+                    name: body.name,
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let newMember = {
+
+                    id: body.id,
+                    department_major: body.department_major, 
+                    name: '', //empty string for the name
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let putOptions = {
+
+                    url: URL + '/members',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(newMember)
+                }
+
+                // Make put request
+                request.put(putOptions, (error, response, body) => {
+
+                    expect(response.statusCode).to.be.equal(400);
+
+                    // Make a get request to verify that the object was not inserted
+                    request.get(URL + '/members', (error, response, body) => {
+
+                        expect(response.statusCode).to.be.equal(200); //If response is successful
+
+                        let list = JSON.parse(body);
+                        let found1 = findMember(list, newMember);
+                        expect(found1).to.be.false; //Check that the member was not updated
+
+                        let found2 = findMember(list, oldMember);
+                        expect(found2).to.be.true; //Check that the member with old values still exists
+                    });
+
+                    done();
+                });                
+            });
         });
 
         // Update the password and make sure it was hashed
         it('Update the password of a member. Make sure it was hashed.', (done) => {
             
-            done();
+            request.get(URL + '/members/1', (error, response, body) => {
+
+                expect(response.statusCode).to.be.equal(200);
+
+                let oldMember = {
+
+                    id: body.id,
+                    department_major: body.department_major,
+                    name: body.name,
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let newMember = {
+
+                    id: body.id,
+                    department_major: body.department_major, 
+                    name: body.name, 
+                    photo_URL: body.photo_URL,
+                    password: body.password + 'other characters' //new password
+                };
+
+                let putOptions = {
+
+                    url: URL + '/members',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(newMember)
+                }
+
+                // Make the put request
+                request.put(putOptions, (error, response, body) => {
+
+                    expect(response.statusCode).to.be.equal(400);
+
+                    // Make a get request to verify that the password was hashed
+                    request.get(URL + '/members/' + oldMember.id, (error, response, body) => {
+
+                        expect(response.statusCode).to.be.equal(200); //If response is successful
+                        let updatedMember = JSON.parse(body);    
+
+                        //Compare the previously stored password and the currently stored password
+                        let areEqualPasswords = areEqualStrings(updatedMember.password, oldMember.password);
+                        //They must be different
+                        expect(areEqualPasswords).to.be.false;
+                    });
+
+                    done();
+                });                
+            });
         });
 
         // Update some or all of the fields of a member. Make sure that the update was successful.
         it('Update some or all of the fields of a member. Make sure that the update was successful.', (done) => {
             
-            done();
+            request.get(URL + '/members/1', (error, response, body) => {
+
+                expect(response.statusCode).to.be.equal(200);
+
+                let oldMember = {
+
+                    id: body.id,
+                    department_major: body.department_major,
+                    name: body.name,
+                    photo_URL: body.photo_URL,
+                    password: body.password
+                };
+
+                let newMember = {
+
+                    id: body.id,
+                    department_major: body.department_major, 
+                    name: body.name + ' other characters',
+                    photo_URL: body.photo_URL + '/photo/123',
+                    password: body.password
+                };
+
+                let putOptions = {
+
+                    url: URL + '/members',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(newMember)
+                }
+
+                // Make a put request
+                request.put(putOptions, (error, response, body) => {
+
+                    expect(response.statusCode).to.be.equal(200);
+
+                    // Make a get request to verify that the object was successfully updated
+                    request.get(URL + '/members', (error, response, body) => {
+
+                        //If response is successful
+                        expect(response.statusCode).to.be.equal(200); 
+
+                        let list = JSON.parse(body);
+                        let found1 = findMember(list, newMember);
+                        //Check that the member was indeed updated
+                        expect(found1).to.be.true; 
+
+                        let found2 = findMember(list, oldMember);
+                        //Check that the member with the old values does not exist anymore
+                        expect(found2).to.be.false; 
+                    });
+
+                    done();
+                });                
+            });
         });
         
     });
@@ -315,10 +513,51 @@ describe('Member model', () => {
         // Cascade deletion should be implemented
         it('Delete an existent member', (done) => {
             
+            // Define POST request parameters and body
+            let postOptions = {
+                url: URL + '/members',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: 'A99999999',
+                    department_major: 'ITC',
+                    name: 'firstName lastName',
+                    photo_URL: 'foto_URL',
+                    password: '12345'
+                })
+            };
+
+            // Make post request to create a new member
+            request.post(postOptions, (error, response, body) => {
+                expect(response.statusCode).to.be.equal(201); // if response is successful
+                let newMember = JSON.parse(body);
+
+                // Make get request to get the inserted object
+                request.get(URL + 'members/' + newMember.id, (error, response, body) => {
+                    expect(response.statusCode).to.be.equal(200); // if response is successful
+                    expect(newMember).to.deep.equal(JSON.parse(body)); // check that the object we created and the one obtain are equal
+                });
+            });
+
+            // Make delete request to delete the inserted member
+            request.delete( URL + '/members/' + postOptions.id, (error, response, body) => {
+                expect(response.statusCode).to.be.equal(200); // if response is successful
+            });
+
+            //Now verify that the items associated to this member (member_project, member_task) were also deleted in the DB
+
+            // Query the instances of member_project of the member that was deleted
+            // request.get();
+
+            // Query the instances of member_task of the member that was deleted
+            // request.get();
+
             done();
         });
 
-        // Delete a non-existent member, response: unsuccesful
+
+        // Try to delete a non-existent member, response: unsuccesful
         it('Delete non existent member', (done) => {
             request.delete(URL + '/members/0', (error, response, body) => {
                 expect(response.statusCode).to.be.equal(400); //response should be unsuccesful
