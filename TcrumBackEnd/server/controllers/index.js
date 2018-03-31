74237f98b6a4e86f0800206adea30654c9360b99
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-03-31 15:11:39 -0600

    Updated routes and controller to include Acceptance_criteria, with basis on most current version on master

--- TcrumBackEnd/server/controllers/index.js
+++ TcrumBackEnd/server/controllers/index.js
@@ -2,18 +2,12 @@ const logs = require('./logs');
 const members = require('./members');
 const projects = require('./projects');
 const tasks = require('./tasks');
-<<<<<<< HEAD
-=======
 const acceptance_criteria = require('./acceptance-criteria');
->>>>>>> model_acceptance-criteria
 
 module.exports = {
     logs,
     members,
     projects,
     tasks,
-<<<<<<< HEAD
-=======
     acceptance_criteria,
->>>>>>> model_acceptance-criteria
 };
