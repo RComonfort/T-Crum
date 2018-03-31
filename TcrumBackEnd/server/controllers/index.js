71de72b8eced7fa3bae576f4db325d81934ef8e2
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-03-31 14:56:24 -0600

    Seeds for Acceptance_criteria were added, routes and controller index were updated to include Acceptance_criteria, taking into consideration the current values in master branch

--- TcrumBackEnd/server/controllers/index.js
+++ TcrumBackEnd/server/controllers/index.js
@@ -1,9 +1,13 @@
-const tasks = require('./tasks');
 const logs = require('./logs');
+const members = require('./members');
+const projects = require('./projects');
+const tasks = require('./tasks');
 const acceptance_criteria = require('./acceptance-criteria');
 
 module.exports = {
-    tasks,
     logs,
-    acceptance_criteria
+    members,
+    projects,
+    tasks,
+    acceptance_criteria,
 };
--- TcrumBackEnd/server/routes/index.js
+++ TcrumBackEnd/server/routes/index.js
@@ -1,6 +1,7 @@
 const tasksController = require('../controllers').tasks;
 const logsController = require('../controllers').logs;
 const sprintsController = require('../controllers').sprints;
+const membersController = require('../controllers').members;
 const acceptance_criteriaController = require('../controllers').acceptance_criteria;
 
 module.exports = (app) => {
@@ -8,7 +9,7 @@ module.exports = (app) => {
     message: 'Welcome to the TCRUM Project API!',
   }));
 
-  //Routes for the TAREAS table
+  //Routes for the TASKS table
   app.post('/api/tasks', tasksController.create);  
   app.get('/api/tasks', tasksController.list);
   app.get('/api/tasks/:id', tasksController.retrieve);
@@ -27,6 +28,20 @@ module.exports = (app) => {
   app.put('/api/sprints/:id', sprintsController.update);
   app.delete('/api/sprints/:id', sprintsController.destroy);
 
+  //Routes for the MEMBERS table
+  app.post('/api/members', membersController.create);
+  app.get('/api/members', membersController.list);
+  app.get('/api/members', membersController.retrieve);
+  app.put('/api/members', membersController.update);
+  app.delete('/api/members', membersController.destroy);
+
+  //Routes for the Project table
+  app.post('/api/projects', tareasController.create);  
+  app.get('/api/projects', tareasController.list);
+  app.get('/api/projects/:id', tareasController.retrieve);
+  app.put('/api/projects/:id', tareasController.update);
+  app.delete('/api/projects/:id', tareasController.destroy);
+  
   //Routes for the ACCEPTANCE_CRITERIA table
   app.post('/api/acceptance-criteria', acceptance_criteriaController.create);  
   app.get('/api/acceptance-criteria', acceptance_criteriaController.list);
