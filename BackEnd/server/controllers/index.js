395f0a7511f0c557abac2161baff99256b2432d1
Tais <taiscecit@hotmail.com>
2018-04-01 15:23:42 -0500

    First commit of technoloy controllers

--- BackEnd/server/controllers/index.js
+++ BackEnd/server/controllers/index.js
@@ -8,6 +8,7 @@ const member_task = require('./member_task');
 const member_project = require('./member_project');
 const project_technology = require('./project_technology');
 const user_stories = require('./user_stories');
+const technology = require('./technology');
 
 module.exports = {
     logs,
@@ -19,5 +20,6 @@ module.exports = {
     member_task,
     member_project,
     project_technology,
-    user_stories
+    user_stories,
+    technology
 };
