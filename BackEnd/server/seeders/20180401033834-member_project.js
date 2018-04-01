a35789a16cb72f8a11034cd895d90014d19a524b
ChavaO777 <salvador@orozco.in>
2018-03-31 22:15:45 -0600

    Deleted the id fields in the 'member_task' and 'member_project' seeders because the fields are auto incremented integers

--- BackEnd/server/seeders/20180401033834-member_project.js
+++ BackEnd/server/seeders/20180401033834-member_project.js
@@ -4,27 +4,22 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('Member_project', [{
-        id: 1,
         member_id: "A00000000",
         project_id: 1
       },
       {
-        id: 2,
         member_id: "A00000000",
         project_id: 2
       },
       {
-        id: 3,
         member_id: "A11111111",
         project_id: 1
       },
       {
-        id: 4,
         member_id: "A22222222",
         project_id: 2
       },
       {
-        id: 5,
         member_id: "A33333333",
         project_id: 3
       }
