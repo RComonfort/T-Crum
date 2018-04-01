f36ceb1438480e8a1a3958ca1ef100f4d37ea6a6
Mónica Pérez Martín <monica.p.m@outlook.com>
2018-04-01 11:13:32 -0500

    Acceptance_criteria seeders fixed

--- BackEnd/server/seeders/20180331204557-acceptance_criteria.js
+++ BackEnd/server/seeders/20180331204557-acceptance_criteria.js
@@ -3,35 +3,26 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Acceptance_criteria', [{
-      duration: 10,
       name: 'Criterion 1',
       type: 'Ac type 1',
       user_story_id: 1
-    }], {});
-
-    return queryInterface.bulkInsert('Acceptance_criteria', [{
-      duration: 5,
+    },
+    {
       name: 'Criterion 2',
       type: 'Ac type 2',
       user_story_id: 2
-    }], {});
-
-    return queryInterface.bulkInsert('Acceptance_criteria', [{
-      duration: 7,
+    },
+    {
       name: 'Completion',
       type: 'Delivery',
       user_story_id: 3
-    }], {});
-
-    return queryInterface.bulkInsert('Acceptance_criteria', [{
-      duration: 1,
+    },
+    {
       name: 'Easy to read',
       type: 'Aesthetics',
       user_story_id: 4
-    }], {});
-
-    return queryInterface.bulkInsert('Acceptance_criteria', [{
-      duration: 5,
+    },
+    {
       name: 'Original',
       type: 'Legal',
       user_story_id: 5
@@ -41,17 +32,17 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Acceptance_criteria', [{
       id: 1
-    }]);
-    return queryInterface.bulkDelete('Acceptance_criteria', [{
+    },
+    {
       id: 2
-    }]);
-    return queryInterface.bulkDelete('Acceptance_criteria', [{
+    },
+    {
       id: 3
-    }]);
-    return queryInterface.bulkDelete('Acceptance_criteria', [{
+    },
+    {
       id: 4
-    }]);
-    return queryInterface.bulkDelete('Acceptance_criteria', [{
+    },
+    {
       id: 5
     }]);
   }
