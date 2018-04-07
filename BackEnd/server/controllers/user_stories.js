2c55220c607b2641659fc644076117442c116894
Pantuflas <antoniojss94@gmail.com>
2018-04-07 18:32:36 -0500

    User story: CRUD rest-api working

--- BackEnd/server/controllers/user_stories.js
+++ BackEnd/server/controllers/user_stories.js
@@ -30,8 +30,8 @@ module.exports = {
         scrum_board_status: req.body.scrum_board_status,
         description: req.body.description,
         priority: req.body.priority,
-        sprint_id: req.params.sprint_id,
-        project_id: req.params.project_id,
+        sprint_id: req.body.sprint_id,
+        project_id: req.body.project_id,
       })
       .then(user_stories => res.status(200).send(user_stories))
       .catch(error => res.status(400).send(error));
@@ -40,7 +40,7 @@ module.exports = {
     return user_stories
             .findAll({
               include: [
-                //{ model: Project, as: 'project' },
+                { model: Project, as: 'project', attributes:  ['id', 'vision', 'name', 'begin_date', 'end_date', 'background', 'risks', 'reach', 'createdAt', 'updatedAt', 'scrum_master_id'] },
                 { model: Sprint, as: 'sprint' }  
               ],
             })
@@ -49,7 +49,7 @@ module.exports = {
   },
   retrieve(req, res){
     // check that id is not null, undefined, not an integer nor 0
-     if(!req.params.id || !Number.isInteger(res.params.id)) { 
+     if(!req.params.id && req.body.id === parseInt(req.body.id,10)) { 
             return res.status(400).send({message: 'The request must contain the parameter id field.'});
           }
 
@@ -57,7 +57,7 @@ module.exports = {
       .findById(req.params.id, {
         include: [
           { model: Sprint, as: 'sprint' },
-          //{ model: Project, as: 'project'},  
+          { model: Project, as: 'project', attributes:  ['id', 'vision', 'name', 'begin_date', 'end_date', 'background', 'risks', 'reach', 'createdAt', 'updatedAt', 'scrum_master_id']},  
         ],
       })
       .then(user_stories => {
@@ -72,11 +72,11 @@ module.exports = {
   },
   update(req, res) {
 
-    if (!req.params.id || !Number.isInteger(res.params.id))
+    if (!req.params.id || (req.params.id === parseInt(req.params.id,10)))
       return res.status(400).send({message: 'attribute id can not be empty and must be an integer.'});
 
-    if (!req.body.weight)
-      return res.status(400).send({message: 'weight attribute can not be empty.'});
+    /*if (!req.body.weight)
+      return res.status(400).send({message: 'weight attribute cant be empty.'});
 
     if (!req.body.scrum_board_status)
       return res.status(400).send({message: 'scrum_board_status attribute can not be empty.'});
@@ -87,15 +87,16 @@ module.exports = {
     if (!req.body.priority)
       return res.status(400).send({message: 'priority attribute can not be empty.'});
 
-    if (!req.body.sprint_id || !Number.isInteger(res.body.sprint_id))
+    if (!req.body.sprint_id && req.body.sprint_id === parseInt(req.body.id,10))
       return res.status(400).send({message: 'sprint_id attribute can not be empty and must be an integer.'});
 
-    if (!req.body.project_id || !Number.isInteger(res.body.project_id))
+    if (!req.body.project_id && req.body.project_id === parseInt(req.body.id,10))
       return res.status(400).send({message: 'project_id attribute can not be empty and must be an integer.'});
-
+*/
 
     return user_stories
       .findById(req.params.id, {
+        include: [ { model: Sprint, as: 'sprint'}]
       })
       .then(user_stories => {
         if (!user_stories) {
@@ -105,12 +106,12 @@ module.exports = {
         }
         return user_stories
           .update({
-            weight: req.body.weight,
-            scrum_board_status: req.body.scrum_board_status,
-            description: req.body.description,
-            priority: req.body.priority,
-            sprint_id: req.params.sprint_id,
-            project_id: req.params.project_id,
+            weight: req.body.weight || user_stories.weight,
+            scrum_board_status: req.body.scrum_board_status || user_stories.scrum_board_status,
+            description: req.body.description || user_stories.description,
+            priority: req.body.priority || user_stories.priority,
+            sprint_id: req.params.sprint_id || user_stories.sprint_id,
+            project_id: req.params.project_id || user_stories.project_id,
           })
           .then(() => res.status(200).send(user_stories))  // Send back the updated tuple.
           .catch((error) => res.status(400).send(error));
