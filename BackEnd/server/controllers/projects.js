e11e154cd4979ff105418fe6faea39a990e30959
DarkHaliax96 <angel.r.ruiz.m@gmail.com>
2018-04-07 17:08:56 -0500

    Fixed technology model. Fixed Project controller

--- BackEnd/server/controllers/projects.js
+++ BackEnd/server/controllers/projects.js
@@ -110,62 +110,54 @@ module.exports = {
     },
 
     update(req, res) {
-        // check that project id is not null, undefined, not an integer or 0
-        if(!req.body.id && req.body.id === parseInt(req.body.id, 10)) { 
-            return res.status(400).send({message: 'ID must be an integer bigger than 0'});
-        }
-        // check that params are not null, undefined or empty string
-        if(!req.body.vision){ 
-            return res.status(400).send({message: 'Attribute vision cannot be empty'});
-        }
-        if(!req.body.name){ 
-            return res.status(400).send({message: 'Attribute name cannot be empty'});
-        }
-        if(!req.body.begin_date){ 
-            return res.status(400).send({message: 'Attribute begin date cannot be empty'});
-        }
-        if(!req.body.end_date){ 
-            return res.status(400).send({message: 'Attribute end date cannot be empty'});
-        }
-
-        var begin = new Date(req.body.begin_date);
-        var end = new Date(req.body.end_date);
-
-        if( begin > end){
-            return res.status(400).send({message: 'End date cannot be before begin date'});
-        }
-        if(!req.body.background){ 
-            return res.status(400).send({message: 'Attribute background cannot be empty'});
-        }
-        if(!req.body.risks){ 
-            return res.status(400).send({message: 'Attribute risks cannot be empty'});
-        }
-        if(!req.body.reach){ 
-            return res.status(400).send({message: 'Attribute reach cannot be empty'});
-        }
-        if(!req.body.scrum_master_id){
-            return res.status(400).send({message: 'Attribute reach cannot be empty'});
-        }
         return Project
             .findById(req.params.id, {
                 attributes : ['id', 'vision', 'name', 'begin_date', 'end_date', 'background', 'risks', 'reach', 'createdAt', 'updatedAt', 'scrum_master_id']
             })
             .then(Project => {
                 if (!Project) {
-                return res.status(400).send({
-                    message: 'Not Found',
-                });
+                    return res.status(400).send({
+                        message: 'Not Found',
+                    });
+                }
+                else{
+                    //If im trying to update any date, the datea must be correct
+                    var begin;
+                    var end;
+                    if(req.body.begin_date && req.body.end_date){
+                        begin = new Date(req.body.begin_date);
+                        end = new Date(req.body.end_date);
+                    }
+                    else{
+                        if(req.body.begin_date){
+                            begin = new Date(req.body.begin_date);
+                            end = new Date(Project.end_date);
+                        }
+                        else{
+                            if(req.body.end_date){
+                                begin = new Date(Project.begin_date);
+                                end = new Date(req.body.end_date);
+                            }
+                            else{
+                                begin = new Date(Project.begin_date);
+                                end = new Date(Project.end_date);
+                            }
+                        }
+                        if( begin > end){
+                            return res.status(400).send({message: 'End date cannot be before begin date'});
+                        }
+                    }
                 }
                 return Project
                 .update({
-                    vision: req.body.vision,
-                    name: req.body.name,
-                    begin_date: req.body.begin_date,
-                    end_date: req.body.end_date,
-                    background: req.body.background,
-                    risks: req.body.risks,
-                    reach: req.body.reach,
-                    scrum_master_id: req.body.scrum_master_id
+                    vision: req.body.vision || Project.vision,
+                    name: req.body.name || Project.name,
+                    begin_date: req.body.begin_date || Project.begin_date,
+                    end_date: req.body.end_date || Project.end_date,
+                    background: req.body.background || Project.background,
+                    risks: req.body.risks || Project.risks,
+                    reach: req.body.reach || Project.reach,
+                    scrum_master_id: req.body.scrum_master_id || Project.scrum_master_id
                 })
                 .then(() => res.status(200).send(Project))  // Send back the updated tuple.
                 .catch((error) => res.status(400).send(error));
