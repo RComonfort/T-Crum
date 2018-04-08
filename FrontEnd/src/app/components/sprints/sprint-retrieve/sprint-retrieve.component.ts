26703171ca39f7a56ee057735d8dcc05041d8f34
Aranzza Abascal <arabascalf@gmail.com>
2018-04-08 16:25:31 -0500

    Added sprint-retrieve component

---
+++ FrontEnd/src/app/components/sprints/sprint-retrieve/sprint-retrieve.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-retrieve',
+  templateUrl: './sprint-retrieve.component.html',
+  styleUrls: ['./sprint-retrieve.component.css']
+})
+export class SprintRetrieveComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
