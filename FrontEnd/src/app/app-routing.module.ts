b35cc581d2769a74689ec5e4b34b76e3e2a010a4
Aranzza Abascal <arabascalf@gmail.com>
2018-04-08 19:02:23 -0500

    Merge

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -7,32 +7,14 @@ import { LoginComponent } from './components/authentication/login/login.componen
 import { AppComponent } from './app.component';
 import { LogoutComponent } from './components/authentication/logout/logout.component';
 import { HomeComponent } from './components/home/home.component';
-<<<<<<< HEAD
 import { SprintRetrieveComponent } from './components/sprints/sprint-retrieve/sprint-retrieve.component';
-
-
 
 const routes: Routes = [
   {path:'login', component: LoginComponent},
   {path: 'logout', component: LogoutComponent},
   {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
   {path:'', component: AppComponent, canActivate: [AuthGuard]},
-  {path: 'sprints/:id', component: SprintRetrieveComponent},
-
-import { TechnologiesListComponent } from './components/technologies/technologies-list/technologies-list.component';
-
-const routes: Routes = [
-  { path:'login', component: LoginComponent},
-  { path: 'logout', component: LogoutComponent},
-  { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-  { path:'', component: AppComponent, canActivate: [AuthGuard]},
-
-  // Technologies
-  { path: 'technologies', component: TechnologiesListComponent, canActivate: [AuthGuard]},
-  
->>>>>>> 6932cb96ad7b99c214e0d8463c640d72e9bcf077
-
-  { path: '**', redirectTo: '' }
+  {path: 'sprints/:id', component: SprintRetrieveComponent}
 ];
 
 @NgModule({
