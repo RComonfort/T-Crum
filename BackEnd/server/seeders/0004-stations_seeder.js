87e209c02f7f1bcac6cab0b54c2f6606052d2fb2
ChavaO777 <salvador@orozco.in>
2018-05-01 11:35:37 -0500

    Added more stations. Now following a circuit.

--- BackEnd/server/seeders/0004-stations_seeder.js
+++ BackEnd/server/seeders/0004-stations_seeder.js
@@ -6,8 +6,8 @@ module.exports = {
     [
       {
         name: 'Estacion 1',
-        lat: 19.113616,
-        lng: -98.247993,
+        lat: 19.129328,
+        lng: -98.262249,
         waiting_people: 3,
         next_crafter_arrival_time: 14.6,
         next_crafter_id: "4543GJDKSM94030",
@@ -16,8 +16,8 @@ module.exports = {
       },
       {
         name: 'Estacion 2',
-        lat: 19.115668,
-        lng: -98.249404,
+        lat: 19.124635,
+        lng: -98.258305,
         waiting_people: 6,
         next_crafter_arrival_time: 24.6,
         next_crafter_id: "4543GJDKSM94030",
@@ -26,14 +26,64 @@ module.exports = {
       },
       {
         name: 'Estacion 3',
-        lat: 19.128059,
-        lng: -98.249993,
+        lat: 19.120632,
+        lng: -98.254359,
         waiting_people: 0,
         next_crafter_arrival_time: 1.6,
         next_crafter_id: "4543GJDKSM94030",
         createdAt: Sequelize.fn('NOW'),
         updatedAt: Sequelize.fn('NOW')
       },
+      {
+        name: 'Estacion 4',
+        lat: 19.118661,
+        lng: -98.252077,
+        waiting_people: 0,
+        next_crafter_arrival_time: 2.6,
+        next_crafter_id: "4543GJDKSM94030",
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW')
+      },
+      {
+        name: 'Estacion 5',
+        lat: 19.121121,
+        lng: -98.249496,
+        waiting_people: 0,
+        next_crafter_arrival_time: 3.6,
+        next_crafter_id: "4543GJDKSM94030",
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW')
+      },
+      {
+        name: 'Estacion 6',
+        lat: 19.123222,
+        lng: -98.247085,
+        waiting_people: 0,
+        next_crafter_arrival_time: 3.6,
+        next_crafter_id: "4543GJDKSM94030",
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW')
+      },
+      {
+        name: 'Estacion 7',
+        lat: 19.126619,
+        lng: -98.251735,
+        waiting_people: 0,
+        next_crafter_arrival_time: 6,
+        next_crafter_id: "4543GJDKSM94030",
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW')
+      },
+      {
+        name: 'Estacion 8',
+        lat: 19.131752,
+        lng: -98.257714,
+        waiting_people: 0,
+        next_crafter_arrival_time: 10,
+        next_crafter_id: "4543GJDKSM94030",
+        createdAt: Sequelize.fn('NOW'),
+        updatedAt: Sequelize.fn('NOW')
+      }
     ], {})
   },
 
@@ -48,6 +98,21 @@ module.exports = {
       },
       {
         id: 3
+      },
+      {
+        id: 4
+      },
+      {
+        id: 5
+      },
+      {
+        id: 6
+      },
+      {
+        id: 7
+      },
+      {
+        id: 8
       }
     ]);
   }
