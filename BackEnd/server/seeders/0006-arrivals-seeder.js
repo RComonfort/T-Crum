1f31d6dcad36762ab5a8ee82f31e80be77092686
ChavaO777 <salvador@orozco.in>
2018-05-01 12:43:39 -0500

    Fixed the crafters and arrivals seeders according to our circuit and present locations of the crafters.

--- BackEnd/server/seeders/0006-arrivals-seeder.js
+++ BackEnd/server/seeders/0006-arrivals-seeder.js
@@ -5,7 +5,8 @@ module.exports = {
 
         return queryInterface.bulkInsert('Arrivals',
 
-            [{
+            [
+                {
                     id: 1,
                     crafter_id: "4543GJDKSM94030",
                     station_id: 1,
@@ -14,15 +15,15 @@ module.exports = {
                 },
                 {
                     id: 2,
-                    crafter_id: "4543GJDKSM94030",
-                    station_id: 2,
+                    crafter_id: "VWLTGH567899",
+                    station_id: 4,
                     createdAt: Sequelize.fn('NOW'),
                     updatedAt: Sequelize.fn('NOW')
                 },
                 {
                     id: 3,
-                    crafter_id: "4543GJDKSM94030",
-                    station_id: 3,
+                    crafter_id: "PNATUF7ARTYS456",
+                    station_id: 7,
                     createdAt: Sequelize.fn('NOW'),
                     updatedAt: Sequelize.fn('NOW')
                 }
